944b684bf55b0cccc2adec263c1476d516cd962c
magrega <56564097+magrega@users.noreply.github.com>
2021-07-18 21:48:29 +0300

    dynamic object url

--- models/product.js
+++ models/product.js
@@ -25,6 +25,7 @@ module.exports = class Product {
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -36,4 +37,11 @@ module.exports = class Product {
     static fetchAll(callBack) {
         getProductsFromFile(callBack);
     }
-}
+
+    static findById(id, callBack){
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            callBack(product);
+        })
+    }
+};
--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,10 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/products/delete');
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/cart', shopController.getCart);
