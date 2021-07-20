75838fff009b196dca42d31e7072a72f587c7b93
magrega <56564097+magrega@users.noreply.github.com>
2021-07-20 21:04:36 +0300

    Add to cart button functionality

--- routes/shop.js
+++ routes/shop.js
@@ -18,6 +18,8 @@ router.get('/orders', shopController.getOrders);
 
 router.get('/cart', shopController.getCart);
 
+router.post('/cart', shopController.postCart);
+
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
