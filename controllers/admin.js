11f17344f889e0d7df5b3bd4e6efdccc2a53a05a
magrega <56564097+magrega@users.noreply.github.com>
2021-07-22 01:25:15 +0300

    Edit product functionality(intro)

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,10 @@
 const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', { pageTitle: 'Add Product',
+    res.render('admin/edit-product', { 
+                                pageTitle: 'Add Product',
                                 path: '/admin/add-product',
+                                editing: false,
                                 formsCSS: true,
                                 activeAddProduct: true,
                                 productCSS: true
@@ -19,6 +21,28 @@ exports.postAddProduct = (req, res, next) => {
     res.redirect('/');
 };
 
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if(!editMode){
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if(!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product,
+            formsCSS: true,
+            activeAddProduct: true,
+            productCSS: true
+        });
+    });
+}
+
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
         res.render('admin/products', {
@@ -27,5 +51,4 @@ exports.getProducts = (req, res, next) => {
             path: '/admin/products'
         })
     }
-        )
-};
+)};
